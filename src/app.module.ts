5b51c0f455de0a54264be5d1c576dc2cdabbcdc1
Arturo Hernández <a.hernandez@gbh.com.do>
2024-03-22 10:42:43 -0400

    Setup MikroORM module in app modules imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
+import { MikroOrmModule } from "@mikro-orm/nestjs";
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { config } from "./config/orm.config";
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), MikroOrmModule.forRoot(config)],
   controllers: [AppController],
   providers: [AppService],
 })
